8793e61c3428a2d5a54f3ce3fae22d20ea9b9748
Anfal <ahassan2@mof.gov.kw>
2022-10-24 18:26:15 +0300

    finished model2

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import date
 
 class user(models.Model):
     Name = models.CharField(max_length=150)
@@ -8,3 +9,11 @@ class user(models.Model):
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=50)
     last_login = models.DateField("Date", auto_now_add=True)
+
+class Event(models.Model):
+    Name = models.CharField(max_length=150)
+    image = models.ImageField()
+    organiser=models.CharField(max_length=150)
+    NumberOfSeats=models.IntegerField()
+    NumberOfBookSeats=models.IntegerField()
+    DateOfEvent=models.DateTimeField(auto_now_add=True)
